6ae7f1e072c3e43c640fed1c437a70114794b4dc
Geethu Krishna <geethu.krishna@ccsdev01.onmicrosoft.com>
2025-11-13 12:03:50 +0530

    Initial Commit

---
+++ src/webparts/taskHistory/components/ITaskHistoryProps.ts
@@ -0,0 +1,7 @@
+export interface ITaskHistoryProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/taskHistory/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITaskHistoryWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TaskHistoryWebPartStrings' {
+  const strings: ITaskHistoryWebPartStrings;
+  export = strings;
+}
